a49a5f7109b1094f9eae8173bb08b70a5d3a8c84
GamePlayRTX <139449036+valery22233445566778899@users.noreply.github.com>
2024-08-20 19:24:12 +0300

    добавил скрипт

--- script.js
+++ script.js
@@ -1,9 +1,26 @@
-// let button = document.querySelector('.start-button')
+//переменные из документа
+let start_button = document.querySelector('start-button')
+// let ans_button1 = document.querySelector('.')
+// let ans_button2 = document.querySelector('.')
+// let ans_button3 = document.querySelector('.')
 
-// button.addEventListener('mouseenter', function() {
-//     anime({
-//         targets: '.start-button',
-//         duration: 500,
-//         scale: 1.2
-//     })
-// })
+//функции
+
+
+//классы
+class Quiz {
+    constructor(question, ans_1, ans_2, ans_3) {
+        this.question = question
+        this.ans_1 = ans_1
+        this.ans_2 = ans_2
+        this.ans_3 = ans_3
+    }
+    //методы
+}
+
+let Quiz = [
+    new Quiz('вопрос', 'ответ1', 'ответ2', 'ответ3'),
+    new Quiz('вопрос', 'ответ1', 'ответ2', 'ответ3'),
+    new Quiz('вопрос', 'ответ1', 'ответ2', 'ответ3')
+]
+
