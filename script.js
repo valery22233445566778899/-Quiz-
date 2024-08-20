a405312beac4413eeb60a336fac32eb6e6a1447a
GamePlayRTX <139449036+valery22233445566778899@users.noreply.github.com>
2024-08-20 18:47:29 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+// let button = document.querySelector('.start-button')
+
+// button.addEventListener('mouseenter', function() {
+//     anime({
+//         targets: '.start-button',
+//         duration: 500,
+//         scale: 1.2
+//     })
+// })
