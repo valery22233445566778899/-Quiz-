e200ad53dd30f5c5df08ca4442631b031e66467e
GamePlayRTX <139449036+valery22233445566778899@users.noreply.github.com>
2024-08-22 20:55:12 +0300

    Update script.js

--- script.js
+++ script.js
@@ -5,6 +5,7 @@ let ans_button2 = document.querySelector('.answer-2')
 let ans_button3 = document.querySelector('.answer-3')
 let doc_picture = document.querySelector('.picture')
 let doc_question = document.querySelector('.question')
+let opinion = []
 
 //функции
 function changeImage(src) {
@@ -13,12 +14,19 @@ function changeImage(src) {
 
 //классы
 class Quiz {
-    constructor(question, ans_1, ans_2, ans_3, picture) {
+    constructor(question, ans_1, ans_2, ans_3, picture_1, picture_2, picture_3) {
         this.question = question
         this.ans_1 = ans_1
         this.ans_2 = ans_2
         this.ans_3 = ans_3
-        this.picture = picture
+        this.picture_1 = picture_1
+        this.picture_2 = picture_2
+        this.picture_3 = picture_3
+        this.answers = [
+            this.ans_1,
+            this.ans_2,
+            this.ans_3
+        ]
     }
     //методы
 
@@ -35,9 +43,37 @@ class Sofa {
 }
 
 let quiz = [
-    new Quiz('вопрос', 'ответ1', 'ответ2', 'ответ3', 'url(*картинка*)'),
-    new Quiz('вопрос', 'ответ1', 'ответ2', 'ответ3', 'url(*картинка*)'),
-    new Quiz('вопрос', 'ответ1', 'ответ2', 'ответ3', 'url(*картинка*)')
+    new Quiz('Куда вы хотите поставить диван?', 
+        'На кухню', 
+        'В гостинную', 
+        'В комнату', 
+        'url(*картинка*)', 
+        'url(*картинка*)', 
+        'url(*картинка*)'),
+
+    new Quiz('Какой формы вы хотите диван?', 
+        'Угловой', 
+        'Прямой', 
+        'Раскладной', 
+        'url(*картинка*)', 
+        'url(*картинка*)', 
+        'url(*картинка*)'),
+
+    new Quiz('Какого размера вы хотите диван?', 
+        'Маленький', 
+        'Средний', 
+        'Большой', 
+        'url(*картинка*)',
+        'url(*картинка*)',
+        'url(*картинка*)'),
+
+    new Quiz('Из кокого материала вы хотите диван?', 
+        'Кожа', 
+        'Натуральное дерево', 
+        'Латекс', 
+        'url(*картинка*)',
+        'url(*картинка*)',
+        'url(*картинка*)')
 ]
 
 const sofas = new Map()
@@ -72,19 +108,24 @@ start_button.addEventListener('click', function() {
     //скрываем элементы на экране
     start_button.style.display = 'none'
 
-    for (let i = 0; i < quiz.length; i += 1) {
+    for (let a = 0; a < quiz.length; a += 1) {
         //Получаем параметры из классов массива
-        let answer_1 = quiz[i].ans_1
-        let answer_2 = quiz[i].ans_2
-        let answer_3 = quiz[i].ans_3
-        let picture = quiz[i].picture
-        let question = quiz[i].question
+        let answer_1 = quiz[a].ans_1
+        let answer_2 = quiz[a].ans_2
+        let answer_3 = quiz[a].ans_3
+        let picture = quiz[a].picture
+        let question = quiz[a].question
         //Применяем полученные параметры к экрану
         ans_button1.innerHTML = answer_1
         ans_button2.innerHTML = answer_2
         ans_button3.innerHTML = answer_3
         doc_picture.innerHTML = picture
         dos_question.innerHTML = question
+        for (let i = 0; i < quiz[a].answers.length; i += 1) {
+            quiz[a].answers[i].addEventListener('click', function () {
+                opinion.push(quiz[a].answers[i])
+            })
+        }
         
     }
 })
